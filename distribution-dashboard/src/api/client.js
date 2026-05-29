a59a5bb417878776d416d8872dd45f887e10a1df
devcore002-Co <devcore002@gmail.com>
2026-05-29 21:40:06 +0000

    Fix API client - remove broken auth/login redirect

--- distribution-dashboard/src/api/client.js
+++ distribution-dashboard/src/api/client.js
@@ -1,22 +1,14 @@
 import axios from 'axios'
-import { useAuthStore } from '../store/auth'
 
 const api = axios.create({
   baseURL: '/api'
-})
-
-api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token
-  if (token) config.headers.Authorization = `Bearer ${token}`
-  return config
 })
 
 api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      useAuthStore.getState().logout()
-      window.location.href = '/login'
+      console.error('Unauthorized request')
     }
     return Promise.reject(err)
   }
